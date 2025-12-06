47d08032b83c33b526dca83224f6c34dca6b2ee0
lbdevDesign <lucasbombaron@gmail.com>
2025-12-06 11:18:33 +0100

    Vercel init

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   integrations: [react()],
+
   vite: {
     plugins: [tailwindcss()],
-  }
+  },
+
+  adapter: vercel()
 });
